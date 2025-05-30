8c9960e603da6d34d9e498a7b9d98ba9ed67f4f1
MattNandavong <146684932+MattNandavong@users.noreply.github.com>
2025-05-30 12:40:19 +1000

    Docs: fix document

---
+++ app/lib/utils/api_constants.dart
@@ -0,0 +1,26 @@
+// iOS simulator:
+// final String baseUrl = 'http://localhost:3300/api';
+
+// Android emulator:
+// final String baseUrl = 'http://10.0.2.2:3300/api';
+
+//Real device
+// final String baseUrl = 'http://192.168.1.101:3300/api';
+
+final String serverIp = 'https://api.connectmytask.xyz';
+
+//publish server
+final String baseUrl = '$serverIp/api';
+
+final String authUrl = '$baseUrl/auth';
+
+final String registerUrl = '$authUrl/register';
+
+final String loginUrl = '$authUrl/login';
+
+final String googleLogin = '$authUrl/google';
+
+final String chatUrl = '$baseUrl/messages';
+
+final String taskUrl = '$baseUrl/tasks';
+
