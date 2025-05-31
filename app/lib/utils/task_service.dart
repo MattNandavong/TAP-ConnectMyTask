a2851f08b8bd1731f08734107ed206d2f5df03f6
MattNandavong <146684932+MattNandavong@users.noreply.github.com>
2025-05-31 21:33:52 +1000

    feat: add app icon

--- app/lib/utils/task_service.dart
+++ app/lib/utils/task_service.dart
@@ -77,6 +77,7 @@ class TaskService {
     final streamedResponse = await request.send();
     final response = await http.Response.fromStream(streamedResponse);
 
+    print(token);
     if (response.statusCode == 201) {
       return jsonDecode(response.body);
     } else {
